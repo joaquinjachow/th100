e7b792c440d1e2e49a4701607e3943589f7751f5
joaquinjachow <joacojachow@hotmail.com>
2024-11-28 11:41:31 -0300

    in construction added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,36 @@
-
 export default function Home() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#f5f5f5",
+        textAlign: "center",
+        padding: "20px",
+        boxSizing: "border-box",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "3rem",
+          color: "#333",
+          marginBottom: "20px",
+        }}
+      >
+        🚧 En Construcción 🚧
+      </h1>
+      <p
+        style={{
+          fontSize: "1.5rem",
+          color: "#555",
+          marginBottom: "40px",
+        }}
+      >
+        Estamos trabajando duro para traerte algo increíble. ¡Vuelve pronto!
+      </p>
     </div>
   );
 }
